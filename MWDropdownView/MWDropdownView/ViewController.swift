7631306eda2a348648059784574b02a5efda543a
WangTeng <woaijia_xxx@126.com>
2015-05-28 10:42:27 +0800

    add example

--- MWDropdownView/MWDropdownView/ViewController.swift
+++ MWDropdownView/MWDropdownView/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
         
         switch r {
         case 0:
-            MWDropdownView.showDropdownView("aaaaaaa", message: nil, bgColor: UIColor.redColor(), textColor: UIColor.whiteColor(), time: 2)
+            MWDropdownView.showDropdownView("aaaaaaa", message: "bbbbbbb", bgColor: UIColor.redColor(), textColor: UIColor.whiteColor(), time: 2)
         case 1:
             MWDropdownView.showDropdownView(nil, message: "2222222", bgColor: UIColor.blueColor(), textColor: UIColor.orangeColor(), time: NO_TIME)
         case 2:
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MWDropdownView
+For example:
+``` swift
+MWDropdownView.showDropdownView("aaaaaaa", message: "bbbbbbb", bgColor:UIColor.redColor(), textColor: UIColor.whiteColor(), time: 2)
+
+MWDropdownView.showDropdownView(nil, message: "2222222", bgColor:UIColor.blueColor(), textColor: UIColor.orangeColor(), time: NO_TIME)
+  			 
+MWDropdownView.show()
+```
